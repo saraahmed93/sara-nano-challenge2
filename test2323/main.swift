9b74d2f16b5ec6cc4d6b096c6982bc781831f150
Sara Ahmed Alshammari <saraahmedalshammari@MacBook-Pro-5.local>
2022-01-02 11:49:14 +0300

    first commit

---
+++ test2323/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  test2323
+//
+//  Created by Sara Ahmed Alshammari on 21/05/1443 AH.
+//
+
+import Foundation
+
+for i in 3...6 {
+print (i)
+}
+
+var count = 0
+while (count < 8) {
+print (count)
+    count++
+}
+//print("Hello, World!")
+
